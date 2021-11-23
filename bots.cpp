f92e2439637d31ccc6d826f671761c9ca9c10394
Злата <zlatatokarenko17@yandex.ru>
2021-11-23 10:11:16 +0300

    не воркает

--- bots.cpp
+++ bots.cpp
@@ -12,6 +12,40 @@ public:
     }
 };
 
+class right_hand_tokarenko_bot:public pc
+{
+public:
+    right_bot(field * p, string n):pc(p, n) {}
+    virtual void ai()
+    {
+        direction face=d;
+        direction hand=x;
+        while (can_go(face) and !can_go(hand)) go(face);
+        hand=clockwise(hand);
+        face=
+
+    }
+private:
+direction clockwise_direction(direction direction q)
+    {
+        if (q==d) return x;
+        if (q==x) return z;
+        if (q==z) return a;
+        if (q==a) return w;
+        if (q==w) return e;
+        if (q==e) return d;
+    }
+direction unclockwise_direction(direction direction q)
+    {
+        if (q==d) return e;
+        if (q==x) return d;
+        if (q==z) return x;
+        if (q==a) return z;
+        if (q==w) return a;
+        if (q==e) return w;
+    }
+};
+
 class panic_bot:public pc
 {
 public:
