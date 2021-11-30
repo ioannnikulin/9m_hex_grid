213eb4ab6e443af6a1cc0454a3670e305b1d3278
OldennOne <qwertybgh@yandex.ru>
2021-11-30 10:09:46 +0300

    bot ready

--- bots.cpp
+++ bots.cpp
@@ -27,9 +27,64 @@ public:
     }
 };
 
+class skorodumov_right_hand_bot: public pc
+{
+public:
+    skorodumov_right_hand_bot(field * p, string n):pc(p, n) {}
+    direction start_dir()
+    {
+        /*vector<int> dirs(6,0);
+
+        if(can_go(a))dirs[0]=1;
+        if(can_go(w))dirs[1]=1;
+        if(can_go(e))dirs[2]=1;
+        if(can_go(d))dirs[3]=1;
+        if(can_go(x))dirs[4]=1;
+        if(can_go(z))dirs[5]=1;
+        for(int i(0);i<6;i++){if(dirs[i]==1)set_dir=i;break;}*/
+        for (auto i: {direction::a, direction::w, direction::e, direction::d, direction::x, direction::z})
+            if (can_go(i)) return i;
+    }
+    direction hand_d(direction gogo)
+    {direction a;
+        if(gogo==direction::a)a=direction::w;
+        if(gogo==direction::w)a=direction::e;
+        if(gogo==direction::e)a=direction::d;
+        if(gogo==direction::d)a=direction::x;
+        if(gogo==direction::x)a=direction::z;
+        if(gogo==direction::z)a=direction::a;
+        return a;
+    }
+    direction left_d(direction gogo)
+    {
+        direction b;
+        if(gogo==direction::a)b=direction::z;
+        if(gogo==direction::w)b=direction::a;
+        if(gogo==direction::e)b=direction::w;
+        if(gogo==direction::d)b=direction::e;
+        if(gogo==direction::x)b=direction::d;
+        if(gogo==direction::z)b=direction::x;
+        return b;
+    }
+    virtual void ai()
+    {
+
+        direction gogo = start_dir();
+        direction left = left_d(gogo);
+        direction hand = hand_d(gogo);
+        while (can_go(gogo)) go(gogo);
+        while(!won()){
+            while(!can_go(gogo) and !can_go(hand)){gogo=left; hand=hand_d(gogo); left=left_d(gogo);}
+            while(can_go(hand)){gogo=hand; hand=hand_d(gogo); left=left_d(gogo);}
+            while(can_go(gogo) and !can_go(hand)) go(gogo);
+        }
+
+    }
+};
+
 void fill_bots(vector<pc*> & bots)
 {
-    bots.push_back(new jenya705::jenya705_bot_starter(NULL));
+    bots.push_back(new skorodumov_right_hand_bot(NULL, "@"));
     bots.push_back(new right_bot(NULL, ">"));
     bots.push_back(new panic_bot(NULL, "?"));
 }
