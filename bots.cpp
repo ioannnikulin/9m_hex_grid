69f2fdc378cd3e577268e7b25981ab2491af077d
Kate <dubovenko-ekaterina@mail.ru>
2021-12-07 10:07:20 +0300

    added dubovenko_righthand_bot

--- bots.cpp
+++ bots.cpp
@@ -1,6 +1,4 @@
 #include "base.hpp"
-#include "jenya705.hpp"
-
 class right_bot:public pc
 {
 public:
@@ -26,10 +24,78 @@ public:
         }
     }
 };
+class dubovenko_righthand_bot:public pc
+{
+private: direction clockwise(direction dir)
+{
+    if(dir==d)
+        return x;
+    if(dir==x)
+        return z;
+    if(dir==z)
+        return a;
+    if(dir==a)
+        return w;
+    if(dir==w)
+        return e;
+    if(dir==e)
+        return d;
+}
+private: direction counterclockwise(direction dir)
+{
+    if(dir==d)
+        return e;
+    if(dir==e)
+        return w;
+    if(dir==w)
+        return a;
+    if(dir==a)
+        return z;
+    if(dir==z)
+        return x;
+    if(dir==x)
+        return d;
+}
+public:
+    dubovenko_righthand_bot(field * p, string n):pc(p, n) {}
+    virtual void ai()
+    {
+        direction face=d;
+        direction hand=clockwise(face);
+        while (can_go(face))
+        {
+           go(face);
+        }
+        while (!won())
+        {
+            /*if(!can_go(face))
+            {
+                face=clockwise(face);
+                hand=clockwise(hand);
+            }*/
+            while ((can_go(face))and(!can_go(hand)))
+            {
+                go(face);
+            }
+            if(can_go(hand))
+            {
+                face=clockwise(face);
+                hand=clockwise(hand);
+            }
+
+            if((!can_go(hand))and(!can_go(face)))
+            {
+                face=counterclockwise(face);
+                hand=counterclockwise(hand);
+            }
+        }
+    }
+};
 
 void fill_bots(vector<pc*> & bots)
 {
-    bots.push_back(new jenya705::jenya705_bot_starter(NULL));
-    bots.push_back(new right_bot(NULL, ">"));
-    bots.push_back(new panic_bot(NULL, "?"));
+    //bots.push_back(new jenya705::jenya705_bot_starter(NULL));
+    //bots.push_back(new right_bot(NULL, ">"));
+    //bots.push_back(new panic_bot(NULL, "?"));
+    bots.push_back(new dubovenko_righthand_bot(NULL, "^"));
 }
