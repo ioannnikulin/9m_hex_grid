0921845b599333913bba8ccf019863fd585a8e14
Кира <kek@kek.lol>
2021-12-07 09:11:20 +0300

    added sidorova_right_hand_bot

--- bots.cpp
+++ bots.cpp
@@ -13,6 +13,66 @@ public:
     }
 };
 
+class sidorova_right_hand_bot:public pc
+{
+public:
+    sidorova_right_hand_bot(field * p, string n):pc(p, n) {}
+    virtual void ai()
+    {
+        direction face=d;
+        direction hand=x;
+    while (can_go(face))
+        {
+            go(face);
+        }
+    while (!won())
+        {
+
+    while (can_go(face) and !can_go(hand))
+    {
+        go(face);
+    }
+    while (can_go(hand))
+    {
+        face = clockwise(face);
+        hand = clockwise(hand);
+    }
+    while(!can_go(face) and !can_go(hand))
+    {
+        face = backwards_clockwise(face);
+        hand = backwards_clockwise(hand);
+    }
+
+    }
+    }
+
+
+private:
+
+    direction clockwise(direction o)
+    {
+    if (o==a) return w;
+    if (o==w) return e;
+    if (o==e) return d;
+    if (o==d) return x;
+    if (o==x) return z;
+    if (o==z) return a;
+    }
+    direction backwards_clockwise(direction o)
+    {
+    if (o==a) return z;
+    if (o==w) return a;
+    if (o==e) return w;
+    if (o==d) return e;
+    if (o==x) return d;
+    if (o==z) return x;
+    }
+
+
+};
+
+
+
 class panic_bot:public pc
 {
 public:
@@ -29,7 +89,6 @@ public:
 
 void fill_bots(vector<pc*> & bots)
 {
-    bots.push_back(new jenya705::jenya705_bot_starter(NULL));
-    bots.push_back(new right_bot(NULL, ">"));
-    bots.push_back(new panic_bot(NULL, "?"));
+
+    bots.push_back(new sidorova_right_hand_bot(NULL, "<3"));
 }
