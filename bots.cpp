2dd9140f2bdf76d2d95e1234bef4670bf213376f
Кира <kek@kek.lol>
2021-12-21 10:07:30 +0300

    does not work

--- bots.cpp
+++ bots.cpp
@@ -35,10 +35,14 @@ public:
     {   int wall (999);
         int fog (9999);
         vector<vector<float>> field_model(50, vector<float> (50, fog));
+
         float cur_dist = dist_to_exit();
         field_model[row][col] = cur_dist;
         direction choice = w;
+
         pair<int,int> choice_coord ({-1,-1});
+        while (!won())
+        {
         choice_coord.first = row + delta(w).first;
         choice_coord.second = col + delta(w).second;
         for (auto i: {direction::w, direction::e, direction::d, direction::x, direction::z, direction::a})
@@ -49,27 +53,39 @@ public:
             {
                 go (i);
                 field_model[row + deltas.first][col + deltas.second] = dist_to_exit();
-                if (field_model[row + deltas.first][col + deltas.second] < field_model[choice_coord.first][choice_coord.second])
+            }
+        go(choice);
+        while (field_model[row + deltas.first][col + deltas.second] < field_model[choice_coord.first][choice_coord.second])
             {
                     choice = i;
-                    choice_coord.first = row + deltas(i).first;
-                    choice_coord.second = col + deltas(i).second;
+                    choice_coord.first = row + delta(i).first;
+                    choice_coord.second = col + delta(i).second;
             }
         }
-        go(choice);
-
+    }
     }
 private:
     int row=25;
     int col=25;
     pair <int, int> delta(direction dir)
     {
-        if (dir == w ) return (-1, -row%2);
-        if (dir == e ) return (-1, +row%2);
-        if (dir == d ) return (+1, 0);
-        if (dir == x ) return (+1, +row%2);
-        if (dir == z ) return (-1, -row%2);
-        if (dir == a ) return (-1, 0);
+        if (dir == w ) return {-1, -row%2};
+        if (dir == e ) return {-1, 1-row%2};
+        if (dir == d ) return {0, +1};
+        if (dir == x ) return {+1, row%2};
+        if (dir == z ) return {+1, row%2+1};
+        if (dir == a ) return {0, -1};
+
+    }
+    direction backwards(direction dir)
+    {
+        if (dir == w) return x;
+        if (dir == e) return z;
+        if (dir == d) return a;
+        if (dir == x) return w;
+        if (dir == a) return d;
+        if (dir == z) return e;
+
 
     }
 };
@@ -77,5 +93,5 @@ private:
 
 void fill_bots(vector<pc*> & bots)
 {
-    bots.a_star_sidorova(new panic_bot(NULL, "0"));
-};
+    bots.push_back(new a_star_sidorova(NULL, "<"));
+}
